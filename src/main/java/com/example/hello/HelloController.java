eb9d8747e8b3c6d321633f3b9bc8eca31149b0e0
Lucas-Marins <lmarins02@hotmail.com.br>
2026-04-29 17:38:24 -0300

    update argocd

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from OpenShift Lucas";
+        return "Hello from OpenShift ArgoCD";
     }
 }

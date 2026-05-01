a8e537f362484d814539efb8a5900ff0f40530df
Lucas-Marins <lmarins02@hotmail.com.br>
2026-04-30 21:31:19 -0300

    subindo release para 1.0.1

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello From Java 1.0.0";
+        return "Hello, World!";
     }
 }

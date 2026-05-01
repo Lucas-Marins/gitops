0aee333d669c6cd79030d435eb9606b933927a3e
Lucas-Marins <lmarins02@hotmail.com.br>
2026-04-30 23:35:00 -0300

    subindo a versao 1.0.6

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Fala ai de baixo paezao";
+        return "Fala ai de baixo paezao teste";
     }
 }

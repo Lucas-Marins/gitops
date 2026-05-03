65d83268cab4895f673637d1c914648e5ee68934
Lucas-Marins <lmarins02@hotmail.com.br>
2026-05-03 17:59:38 -0300

    subindo versoes

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Subindo versao 1.0.1";
+        return "Hello from versão 1.0.5";
     }
 }

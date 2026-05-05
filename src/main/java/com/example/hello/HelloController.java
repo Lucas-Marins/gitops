914bedfbc5a6fc75a7fcb07b6d755d0f42bf4610
Lucas Marins <lmarins02@hotmail.com.br>
2026-05-05 07:02:47 -0300

    subindo versao java

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from versão 1.0.2 do Workshop de GitOps!";
+        return "Hello from versão 1.0.3 do Workshop de GitOps!";
     }
 }
